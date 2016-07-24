cc56b8fe06e1507ddf29c9d1c495e76e6e2f8ce5
Noah Lindner <noah.lindner@realself.com>
2016-07-24 13:19:01 -0700

    Some more progress, assigning docs back to topics

--- get_topics.py
+++ get_topics.py
@@ -28,14 +28,17 @@ def load_reviews(filename, field):
             if word not in result:
                 result[word] = 0
             result[word] += 1
-        return result 
+        return result
+
+    def clean(doc):
+        return doc[field].replace('\r\n', '').lower()
 
 
-    def process(review):
-        cleaned = review[field].replace('\r\n', '')
+    def process(document):
+        cleaned = clean(document)
 
         # tokenize
-        tokens = tokenizer.tokenize(cleaned.lower())
+        tokens = tokenizer.tokenize(cleaned)
 
         def remove_stopwords(word):
             return len(word) >= 3 and word not in get_stop_words('en')
@@ -53,11 +56,13 @@ def load_reviews(filename, field):
 
     progress = progressbar.ProgressBar()
 
-    review_bodies = [process(s) for s in progress(review_json)]
+    cleaned_docs = filter(clean, review_json)
 
-    return review_bodies
+    documents = [process(s) for s in progress(cleaned_docs)]
 
-
+    # It is possible that the process() step generated some empty documents, 
+    # remove those
+    return filter(lambda x: len(x) > 0, documents), cleaned_docs
 
 # Return map from words->index
 def generate_vocabulary(reviews):
@@ -88,20 +93,20 @@ def build_document_term_matrix(reviews, vocabulary):
 if __name__ == "__main__":
 
     print "Load file"
-    reviews = load_reviews('./question-titles-md.json', 'title')
+    word_counts, documents = load_reviews('./review-titles-lg.json', 'title')
 
     print "Generate vocabulary"
-    vocabulary = generate_vocabulary(reviews)
+    vocabulary = generate_vocabulary(word_counts)
 
-    print "Got a corupus of %d documents" % len(reviews)
+    print "Got a corupus of %d documents" % len(word_counts)
     print "Got a vocab of %d words" % len(vocabulary)
 
     print "Building doc term thing..."
     vocabulary_with_index = {word: index for index, word in enumerate(vocabulary)}
 
-    doc_term_matrix = build_document_term_matrix(reviews, vocabulary_with_index)
+    doc_term_matrix = build_document_term_matrix(word_counts, vocabulary_with_index)
 
-    for num_topics in (50, 100):
+    for num_topics in (50,):
         print "Making model with %d topics" % num_topics
         model = lda.LDA(n_topics=num_topics, n_iter=500, random_state=1)
         model.fit(doc_term_matrix)
@@ -112,4 +117,13 @@ if __name__ == "__main__":
             topic_words = np.array(vocabulary)[np.argsort(topic_dist)][:-(n_top_words+1):-1]
             print('Topic {}: {}'.format(i, ' '.join(topic_words)))
 
+
+        # counts = {i: 0 for i in range(num_topics)}
+
+        # for i, doc_topic in enumerate(model.doc_topic_):
+        #     top_topic = doc_topic.argmax()
+        #     # print("{} (top topic: {})".format(documents[i], doc_topic[i].argmax()))
+        #     counts[top_topic] += 1
+
+        # print counts
     
